1dc823e6bf6b650bb6fafd49b6ba6dc8469ca222
oleg <o.novokhatsky@gmail.com>
2025-05-19 20:48:07 -0700

    Merge remote-tracking branch 'origin/v1.1' into v1.1
    # Conflicts:
    #	electron-react-app/src/components/SettingsPanel.jsx

--- electron-react-app/src/components/SettingsPanel.jsx
+++ electron-react-app/src/components/SettingsPanel.jsx
@@ -7,6 +7,7 @@ export function SettingsPanel({
                                   setUrl,
                                   reloadWebview,
                                   oscPort,
+                                  setOscPort,
                                   applyPort,
                                   oscLog,
                                   onClose,
